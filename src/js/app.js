4d299758df6c86f1415bbe4c17f343064d9a141d
Iraida <ira.serq.1996@gmail.com>
2023-06-27 16:08:53 +0300

    добавила адаптив

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as swiper from "./modules/swiper.js";
+import * as swiper1 from "./modules/swiper.js";
 
 flsFunctions.isWebp();
+swiper1.swiper1();
 swiper.swiper();
--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -3,7 +3,6 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 
 export function swiper() {
-
     const slider = new Swiper('.swiper', {
         maxwidth: 1280,
         slidesPerView: 1,
@@ -14,8 +13,26 @@ export function swiper() {
             renderBullet: function (index, className) {
                 return '<span class="' + className + '">' + (index + 1) + "</span>";
             }
-        },
+        }
+    });
+}
+
+export function swiper1() {
+    const slider1 = new Swiper('.swiper-1', {
         breakpoints: {
+            695: {
+                slidesPerView: 'auto',
+                spaceBetween: 32,
+            },
+            280: {
+                maxwidth: 296,
+                slidesPerView: 1,
+                spaceBetween: 32,
+                pagination: {
+                    el: '.swiper-pagination-1',
+                    clickable: true
+                },
+            }
         }
     });
 }
